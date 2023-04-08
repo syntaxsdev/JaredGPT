ef40aff2efde7db877d6e23bcd1f422cce6d5d7c
syntaxsdev <sidneycglinton@gmail.com>
2023-04-08 18:30:46 -0400

    fixed communication error if user messaged in a DM

--- Webhooks/iGPT/main.py
+++ Webhooks/iGPT/main.py
@@ -32,9 +32,15 @@ async def read_root():
 
 @app.post("/messages/")
 async def post_message(msg: Message):
+    print(msg)
     controller = GPTController()
-    qst: str = msg.body['message'][5:] 
-    outgoing = OutgoingMessage(body={"message": controller.getResponse(question=qst)}, recipient={"handle": msg.recipient['handle']})
+    qst: str = msg.body['message'][5:]
+    recipient = msg.sender['handle']
+    # If it is a group chat
+    if str(msg.recipient['handle']).find("iMessage;+;chat") > -1 or (msg.sender['isMe'] == True):
+        recipient = msg.recipient['handle']
+
+    outgoing = OutgoingMessage(body={"message": controller.getResponse(question=qst)}, recipient={"handle": recipient})
     requests.post(url=os.getenv('JaredEndpoint'), json=outgoing.dict())
 
     #Delete the object
